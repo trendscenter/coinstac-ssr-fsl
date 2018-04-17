8b1256238acf0faabd13af370383f126319f7a27
Eric Verner <eric.verner@datalyticsolutions.com>
2018-04-17 12:46:48 -0600

    Added first real test

--- test/test_regression.py
+++ test/test_regression.py
@@ -1,7 +1,15 @@
 """ Tests for local_ancillary.py """
 
 import pytest
+import numpy as np
+import os
+import sys
+
+file_dir_path = os.path.dirname(os.path.realpath(__file__))
+project_path = os.path.dirname(file_dir_path)
+sys.path.append(project_path)
 import regression as reg
+
 
 
 def test_one_shot_regression_normal():
@@ -16,7 +24,10 @@ def test_one_shot_regression_bad_input():
 
 def test_y_estimate_normal():
     """ Tests that regression.y_estimate works for expected input """
-    pass
+    biased_X = np.array([[1, 0], [2, 0], [3, 0]])
+    beta_vector = [1, 0]
+    expected = np.array([1, 2, 3])
+    assert np.array_equal(reg.y_estimate(biased_X=biased_X, beta_vector=beta_vector), expected)
 
 
 def test_y_estimate_bad_input():
