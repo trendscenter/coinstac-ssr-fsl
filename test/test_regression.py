e5fb66561cbe4e6e71716620c71633c79050dc1d
Eric Verner <eric.verner@datalyticsolutions.com>
2018-04-17 15:20:04 -0600

    Added tests for one_shot_regression and y_estimate

--- test/test_regression.py
+++ test/test_regression.py
@@ -14,12 +14,19 @@ import regression as reg
 
 def test_one_shot_regression_normal():
     """ Tests that regression.one_shot_regression works for expected input """
-    pass
+    X = np.array([1, 2, 3, 4])
+    y = [1, 2, 3, 4]
+    expected_beta_vector = np.array([1.])
+    np.testing.assert_allclose(reg.one_shot_regression(X, y, 0), expected_beta_vector)
 
 
 def test_one_shot_regression_bad_input():
     """ Tests that regression.one_shot_regression fails for bad input """
-    pass
+    X = np.array([1, 2, 3, 4])
+    y = [1, 2, 3,]
+    expected_beta_vector = np.array([1.])
+    with pytest.raises(ValueError):
+        assert reg.one_shot_regression(X, y, 0)
 
 
 def test_y_estimate_normal():
@@ -31,7 +38,10 @@ def test_y_estimate_normal():
 
 
 def test_y_estimate_bad_input():
-    """ Tests that regression.y_estimate fails for bad input """
-    pass
+    """ Tests that regression.y_estimate raises ValueError when array dimensions do not match """
+    biased_X = np.array([[1, 0, 1], [2, 0, 1], [3, 0, 1]])
+    beta_vector = [1, 0]
+    with pytest.raises(ValueError):
+        assert reg.y_estimate(biased_X=biased_X, beta_vector=beta_vector)
 
 
