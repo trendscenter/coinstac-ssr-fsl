aeefd314cf63da61929a3980812baa91f16aceb4
hvgazula <hvgazula@umich.edu>
2018-05-04 23:09:13 -0600

    corrected a typo in code

--- regression.py
+++ regression.py
@@ -17,7 +17,7 @@ with warnings.catch_warnings():
 def list_recursive(d, key):
     for k, v in d.items():
         if isinstance(v, dict):
-            for found in listRecursive(v, key):
+            for found in list_recursive(v, key):
                 yield found
         if k == key:
             yield v
