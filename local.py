Corrected global rsquared statistic computation

local.py
@@ -57,7 +57,8 @@ def local_1(args):
 
     lamb = input_list["lambda"]
 
-    beta_vector, local_stats_list, meanY_vector, lenY_vector = local_stats_to_dict_fsl(X, y)
+    beta_vector, local_stats_list, meanY_vector, lenY_vector = local_stats_to_dict_fsl(
+        X, y)
 
     output_dict = {
         "beta_vector_local": beta_vector,
@@ -130,8 +131,7 @@ def local_2(args):
 
         X_, y_ = ignore_nans(biased_X, curr_y)
 
-        SSE_local.append(
-            reg.sum_squared_error(X_, y_, avg_beta_vector[index]))
+        SSE_local.append(reg.sum_squared_error(X_, y_, avg_beta_vector[index]))
         SST_local.append(
             np.sum(np.square(np.subtract(y_, mean_y_global[index]))))
 

local_ancillary.py
@@ -70,12 +70,13 @@ def local_stats_to_dict_vbm(X, y):
 
     pvalues = 2 * sp.stats.t.sf(np.abs(tvalues), dof_global)
 
-    keys = ["beta", "sse", "pval", "tval", "rsquared"]
+    keys = ["beta", "sse", "tval", "pval", "rsquared"]
 
     values1 = pd.DataFrame(
         list(
-            zip(params.T.tolist(), sse.tolist(), pvalues.T.tolist(),
-                tvalues.T.tolist(), rsquared.tolist())),
+            zip(params.T.tolist(),
+                sse.tolist(),
+                tvalues.T.tolist(), pvalues.T.tolist(), rsquared.tolist())),
         columns=keys)
 
     local_stats_list = values1.to_dict(orient='records')
@@ -136,15 +137,15 @@ def local_stats_to_dict_fsl(X, y):
         local_sse.append(model.ssr)
         local_pvalues.append(model.pvalues)
         local_tvalues.append(model.tvalues)
-        local_rsquared.append(model.rsquared_adj)
+        local_rsquared.append(model.rsquared)
 
-    keys = ["beta", "sse", "pval", "tval", "rsquared", "covariate_labels"]
+    keys = ["beta", "sse", "tval", "pval", "rsquared", "covariate_labels"]
     local_stats_list = []
 
     for index, _ in enumerate(y_labels):
         values = [
             local_params[index].tolist(), local_sse[index],
-            local_pvalues[index].tolist(), local_tvalues[index].tolist(),
+            local_tvalues[index].tolist(), local_pvalues[index].tolist(),
             local_rsquared[index], X_labels
         ]
         local_stats_dict = {key: value for key, value in zip(keys, values)}

remote.py
@@ -61,7 +61,9 @@ def remote_1(args):
         np.array(input_list[site]["count_local"]) for site in input_list
     ]
     mean_y_global = np.array(mean_y_local) * np.array(count_y_local)
-    mean_y_global = np.average(mean_y_global, axis=0)
+    mean_y_global = np.sum(
+        mean_y_global, axis=0) / np.sum(
+            count_y_local, axis=0)
 
     dof_global = sum(count_y_local) - avg_beta_vector.shape[1]
 